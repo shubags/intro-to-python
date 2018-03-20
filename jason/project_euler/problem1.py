f67aa0cad960d87a8f57f99e6910ac1c4d39f339
shubags <js@a.ki>
2018-03-20 10:59:04 -0700

    updated ranking of the number

--- jason/project_euler/problem1.py
+++ jason/project_euler/problem1.py
@@ -1,2 +1,2 @@
 my_sum = sum([i for i in range(1000) if ((i % 3 == 0) | (i % 5 == 0))])
-print '{} is the greatest number of all time'.format(my_sum)
+print '{} is the third greatest number of all time'.format(my_sum)
