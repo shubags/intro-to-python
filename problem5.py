3a0a053ee67b89e85d397e60447c60e45b919a22
pvazquez93 <pvazquez@a.ki>
2018-04-05 16:25:48 -0500

    New algorithm and PEP8 style guide compliance

--- problem5.py
+++ problem5.py
@@ -2,31 +2,74 @@
 Created on Sun Mar 18 10:34:22 2018
 @author: patricio
 
-is_divisible_by: given a number and a list of numbers, it returns true if the
-number is divisible by all the numbers in the list or false if not.
+2520 is the smallest number that can be divided by each of the numbers from
+1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all
+of the numbers from 1 to 20?
+
+Since not all numbers from 1 to 20 (lets call it k) are primes and those numbers can be
+expressed as a product of prime numbers, our approach consists in first finding
+all prime numbers below k and the exponents attached to each of the prime numbers
+which can lead to produce any of the numbers between 1 and k. Then the product of
+those numbers (exponent included) is the smallest number that we're looking.
+For instance if k = 4 meaning that I want to find the smallest number divisible
+by numbers 1 trhough 4, solution would be: 2*3*2 = 12. Noticing that 4 is implied
+in the 2*2 multiplication. So in this case the exponent of 2 would be 2.
+
+Getting back to the problem since for instance 5^2 = 25, and 25>20 we can
+generalize and limit our search till squared root of k. The rest of prime numbers
+will have exponent equals to 1.
+
+find_smallest_num() in conjunction with find_all_primes_below() produce the
+smallest number that can be divisible from numbers 1 through 'lim_divsor'.
 """
+
 import time
+import math
 
-def is_divisible_by(number, list_of_numbers):
-    default = True
-    for num in list_of_numbers:
-        if number % num != 0:
-            default = False
-            break
-    return default
-    
-start = time.time()    
-    
-num = 2520
-condition_meet = False
-lom = list(range(1,21))
 
-while(condition_meet==False):
-    if is_divisible_by(num, lom):
-        condition_meet = True
-    else:
-        num += 2
+def multiply(numbers):
+    total = 1
+    for num in numbers:
+        total *= num
+    return total
 
-print(num)
 
+def find_all_primes_below(n):
+    prime_list = [True for i in range(n)]
+    p = 2
+    condition_meet = False
+    while condition_meet is False:
+        nums_crossed = 0
+        if prime_list[p]:
+            for i in range(p*2, n, p):
+                if prime_list[i] is not False:
+                    prime_list[i] = False
+                    nums_crossed += 1
+        # if for thee previous prime all numbers have been crossed (meaning
+        # numbers_crossed variable is equal to 0) then all
+        # remaining numbers are prime
+        if nums_crossed < 1:
+            condition_meet = True
+        else:
+            p += 1
+    list_of_primes = [i for i in range(2,n) if prime_list[i] == True]
+    return list_of_primes
+
+
+def find_smallest_num(lim_divisor):
+    primes = find_all_primes_below(20)
+    num_primes = len(primes)
+    exp_vec = [1]*num_primes
+    lim = int(math.sqrt(lim_divisor)+1)
+    i = 0
+    while primes[i] <= lim:
+        exp_vec[i] = math.floor(math.log(lim_divisor)/math.log(primes[i]))
+        i += 1
+    smallest_num = multiply([primes[i]**exp_vec[i] for i in range(num_primes)])
+    return smallest_num
+
+
+start = time.time()
+print(find_smallest_num(20))
 print(time.time()-start)
