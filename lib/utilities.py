1f8a511e7b8ed6f42c59614c7200423dca508f8d
pvazquez93 <pvazquez@a.ki>
2018-04-05 17:06:26 -0500

    Added function find_all_primes_below() to the utilities module.

--- lib/utilities.py
+++ lib/utilities.py
@@ -8,3 +8,24 @@ def sum_if_mult(nums, div_lst):
         sum_result = sum([i for i in nums if i % div_lst == 0])
     return sum_result
 
+
+def find_all_primes_below(n):
+    prime_list = [True for i in range(n)]
+    p = 2
+    condition_meet = False
+    while condition_meet is False:
+        nums_crossed = 0
+        if prime_list[p]:
+            for i in range(p*2, n, p):
+                if prime_list[i] is not False:
+                    prime_list[i] = False
+                    nums_crossed += 1
+        # if for thee previous prime all numbers have been crossed (meaning
+        # numbers_crossed variable is equal to 0) then all
+        # remaining numbers are prime
+        if nums_crossed < 1:
+            condition_meet = True
+        else:
+            p += 1
+    list_of_primes = [i for i in range(2,n) if prime_list[i] == True]
+    return list_of_primes
