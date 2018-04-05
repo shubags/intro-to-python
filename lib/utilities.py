77b0254d26d6b2577b2916d271a2cf643cf8fd1f
pvazquez93 <pvazquez@a.ki>
2018-04-05 16:55:23 -0500

    Changes in sum_if_mult function to accept a sole integr or a list.

--- lib/utilities.py
+++ lib/utilities.py
@@ -1,8 +1,10 @@
 # This module contain useful functions to solve the problems
 
 
-def sum_if_mult(nums, div_nums):
-    div_lst = list(div_nums)
-    sum_result = sum([i for i in range(nums) if any([i % num == 0 for num in div_lst])])
+def sum_if_mult(nums, div_lst):
+    if type(div_lst) == list:
+        sum_result = sum([i for i in range(nums) if any([i % num == 0 for num in div_lst])])
+    else:
+        sum_result = sum([i for i in range(nums) if i % div_lst == 0])
     return sum_result
 
