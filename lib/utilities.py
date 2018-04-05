d7938e635c9f429c87d0de0993bf589d7578daa4
pvazquez93 <pvazquez@a.ki>
2018-04-05 16:59:51 -0500

    To receive a list of numbers instead of a single number used to build a list from 0 to that number.

--- lib/utilities.py
+++ lib/utilities.py
@@ -3,8 +3,8 @@
 
 def sum_if_mult(nums, div_lst):
     if type(div_lst) == list:
-        sum_result = sum([i for i in range(nums) if any([i % num == 0 for num in div_lst])])
+        sum_result = sum([i for i in nums if any([i % num == 0 for num in div_lst])])
     else:
-        sum_result = sum([i for i in range(nums) if i % div_lst == 0])
+        sum_result = sum([i for i in nums if i % div_lst == 0])
     return sum_result
 
