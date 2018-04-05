d8a4246e8339833220040a369735d10e74c55f3c
pvazquez93 <pvazquez@a.ki>
2018-04-05 17:08:38 -0500

    Utilities module adaptation.

--- problem1.py
+++ problem1.py
@@ -2,9 +2,6 @@
 Created on Fri Mar 16 22:24:40 2018
 @author: patricio
 
-Description:
-This function calculates the sum of all multiples of 3 or 5 for a N number
-of numbers
 """
 
 from lib import utilities as utl
